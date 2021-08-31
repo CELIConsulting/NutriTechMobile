0c3ef1dcf3e0b1a5b0cb70fc8a1e62bfa25023e2
Emmanuel <emmanuelfernandez600@gmail.com>
2021-08-30 23:40:34 -0300

    inicializando componentes UI

--- app/src/main/java/com/istea/nutritechmobile/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/istea/nutritechmobile/ui/activity/LoginActivity.kt
@@ -2,12 +2,32 @@ package com.istea.nutritechmobile.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
 import com.istea.nutritechmobile.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var etLoginMail: EditText
+    private lateinit var etLoginPassword: EditText
+    private lateinit var btnLogin: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_NutriTechMobile)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupUI()
     }
+
+    //TODO Inicializar componentes de la pantalla de login
+    private fun setupUI() {
+        etLoginMail = findViewById(R.id.etLoginMail)
+        etLoginPassword = findViewById(R.id.etLoginPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            //TODO login()
+        }
+
+    }
+
 }
