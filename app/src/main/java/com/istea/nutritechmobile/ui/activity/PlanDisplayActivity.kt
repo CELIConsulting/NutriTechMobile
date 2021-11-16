81186178accce5f67022339bf73797f655ed099b
Emmanuel <emmanuelfernandez600@gmail.com>
2021-11-16 06:53:35 -0300

    Fix al bug que rompia la pantalla que muestra el plan y se retoco la UI

--- app/src/main/java/com/istea/nutritechmobile/ui/activity/PlanDisplayActivity.kt
+++ app/src/main/java/com/istea/nutritechmobile/ui/activity/PlanDisplayActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.api.Distribution
 import com.istea.nutritechmobile.R
 import com.istea.nutritechmobile.data.Plan
+import com.istea.nutritechmobile.firebase.FirebaseAuthManager
 import com.istea.nutritechmobile.firebase.FirebaseFirestoreManager
 import com.istea.nutritechmobile.model.PlanDisplayRepositoryImp
 import com.istea.nutritechmobile.presenter.PlanDisplayPresenterImp
 import com.istea.nutritechmobile.presenter.interfaces.IPlanDisplayPresenter
 import com.istea.nutritechmobile.ui.interfaces.IPlanDisplayView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class PlanDisplayActivity : AppCompatActivity(), IPlanDisplayView {
@@ -29,12 +35,19 @@ class PlanDisplayActivity : AppCompatActivity(), IPlanDisplayView {
     private lateinit var Meriendatv: TextView
     private lateinit var Cenatv: TextView
     private lateinit var Colaciontv: TextView
+    private lateinit var contenedorSinPlan: LinearLayout
     private lateinit var toolbar: Toolbar
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var scrollView: ScrollView
     private val planDisplayPresenter: IPlanDisplayPresenter by lazy {
         PlanDisplayPresenterImp(this, PlanDisplayRepositoryImp(FirebaseFirestoreManager(this)))
     }
-    private val noData = "-- No data --"
+
+    private val fireAuthManager: FirebaseAuthManager by lazy {
+        FirebaseAuthManager()
+    }
+
+    private val noData = "-- No hay información para este campo --"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,18 +80,23 @@ class PlanDisplayActivity : AppCompatActivity(), IPlanDisplayView {
         Meriendatv = findViewById(R.id.Meriendatv)
         Cenatv = findViewById(R.id.Cenatv)
         Colaciontv = findViewById(R.id.Colaciontv)
+        scrollView = findViewById(R.id.scrollView)
+        contenedorSinPlan = findViewById(R.id.noPlanContainer)
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         setupToolbar()
 
-        lifecycleScope.launch {
-            planDisplayPresenter.fillPlanInfo(intent.getStringExtra("Email"))
+        lifecycleScope.launch(Dispatchers.Main) {
+            planDisplayPresenter.fillPlanInfo(fireAuthManager.getAuthEmail())
         }
     }
 
     override suspend fun fillDataView(dataRepo: Plan?) {
+
+        //Ocultar campos y mostrar el aviso de que no hay plan cargado
         if (dataRepo != null) {
-            Nombretv.text = dataRepo.Nombre
-            Tipotv.text = dataRepo.Tipo
+            showPlanAlimentacion()
+            Nombretv.text = dataRepo.Nombre.uppercase()
+            Tipotv.text = dataRepo.Tipo.uppercase()
 
             CantAguaDiariaTv.text = dataRepo.CantAguaDiaria.toString()
             CantColacionesDiariasTv.text = dataRepo.CantColacionesDiarias.toString()
@@ -107,7 +125,19 @@ class PlanDisplayActivity : AppCompatActivity(), IPlanDisplayView {
             } else {
                 Colaciontv.text = this.noData
             }
+        } else {
+            hidePlanAlimentacion()
         }
+    }
+
+    private fun hidePlanAlimentacion() {
+        scrollView.isVisible = false
+        contenedorSinPlan.isVisible = true
+    }
+
+    private fun showPlanAlimentacion() {
+        contenedorSinPlan.isVisible = false
+        scrollView.isVisible = true
     }
 
     private fun goToDailyRegistry() {
